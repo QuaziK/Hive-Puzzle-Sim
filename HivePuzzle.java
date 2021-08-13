e51454508624b31de8a44cfc67743fb5fd8cd848
QuaziK <ykozhevn@gmail.edu>
2021-08-13 12:34:11 -0400

    Initial commit

---
+++ HivePuzzle.java
@@ -0,0 +1,180 @@
+import java.util.*;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+class Tuple<X, Y> { 
+    public final X x; 
+    public final Y y; 
+    public Tuple(X x, Y y) { 
+        this.x = x; 
+        this.y = y; 
+    } 
+    public String toString(){
+        return "(" + x + ", " + y + ")";
+    }
+}
+
+class HiveTile {
+    char[] logotypes;
+    Icon[] icontypes;
+    char display;
+    int deffault;
+    int num;
+    
+    public HiveTile(char[] l){
+        this.logotypes = l;
+        this.num = 0;
+        this.deffault = num;
+    }
+    
+    public HiveTile(char[] l, int nn){
+        this.logotypes = l;
+        this.num = nn;
+        this.deffault = num;
+    }
+     
+    public HiveTile(Icon[] ia){
+        this.icontypes = ia;
+        this.num = 0;
+        this.deffault = num;        
+    }
+     
+    public void goNext(){
+        num++;
+        if (num > 3) {
+            num = 0;
+        }
+    }
+    
+    public void goPrevious(){
+        num--;
+        if (num < 0) {
+            num = 3;
+        }
+    }
+    
+    public void setSeed(int nn){
+        num = nn;
+        deffault = num;
+    }
+    
+    public void resetToDeffault(){
+        num = deffault;
+    }
+    
+    public String view(){
+        display = logotypes[this.num];
+        return display + "";
+    }
+    
+    public Icon viewIcon(){
+        return icontypes[this.num];
+    }
+}
+
+public class HivePuzzle {
+    
+    Icon barsIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\bars.png");
+    Icon AcrossIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\Across.png");
+    Icon radioIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\radio.png");
+    Icon bugIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\bug.png");
+    
+    Icon[] icontypes = {barsIcon,AcrossIcon,radioIcon,bugIcon};
+    char[] logotypes = {404,412,415,418};
+    HiveTile[] board = {new HiveTile(icontypes), new HiveTile(icontypes), new HiveTile(icontypes),
+                        new HiveTile(icontypes), new HiveTile(icontypes), new HiveTile(icontypes),
+                        new HiveTile(icontypes), new HiveTile(icontypes), new HiveTile(icontypes)};
+    
+    String seed = "000000000";
+    
+    ArrayList<Tuple<Integer, Integer>> moves = new ArrayList<Tuple<Integer, Integer>>();
+
+    //TODO: update the rest of the tiles with increment
+    public void makeMove(int x, int y){
+        Tuple<Integer, Integer> t = new Tuple<Integer,Integer>(x, y);
+        moves.add(t);
+        int target = matrixToLinear(x,y);
+        int[] collat = {target, target+3,target+6,target-3,target-6, -1, -1};
+        switch (target%3) {
+            case 0:
+                collat[5] = target + 1;
+                collat[6] = target + 2;
+                break;
+            case 1:
+                collat[5] = target + 1;
+                collat[6] = target - 1;
+                break;
+            case 2:
+                collat[5] = target - 1;
+                collat[6] = target - 2;
+                break;
+        }
+        for (int ii = 0; ii<collat.length; ii++){
+            try {
+                board[collat[ii]].goNext();
+            } catch (IndexOutOfBoundsException E) {
+                // do nothing
+            }
+        }
+    }
+    
+    public void resetSeed(){
+        char[] seedc = seed.toCharArray();
+        for (int ii = 0; ii < 9; ii++){
+            board[ii].resetToDeffault();
+        }
+    }
+    
+    public void newSeed(){
+        char[] newseed = generateSeed().toCharArray();
+        for (int ii = 0; ii < 9; ii++){
+            board[ii].setSeed((int)newseed[ii]);
+        }
+    }
+    
+    private String generateSeed(){
+        char[] outc = new char[9];
+        for (int ii = 0; ii < 9; ii++){
+            outc[ii] = (char)(int)Math.floor(Math.random()*(3-0+1)+0);
+        }
+        return String.valueOf(outc);
+    }
+    
+    //TODO: all of undo
+    public void undo(){
+        Tuple<Integer,Integer> t = moves.get(moves.size()-1);
+        moves.remove(moves.size()-1);
+        int target = matrixToLinear(t.x,t.y);
+        int[] collat = {target, target+3,target+6,target-3,target-6, -1, -1};
+        switch (target%3) {
+            case 0:
+                collat[5] = target + 1;
+                collat[6] = target + 2;
+                break;
+            case 1:
+                collat[5] = target + 1;
+                collat[6] = target - 1;
+                break;
+            case 2:
+                collat[5] = target - 1;
+                collat[6] = target - 2;
+                break;
+        }
+        for (int ii = 0; ii<collat.length; ii++){
+            try {
+                board[collat[ii]].goPrevious();
+            } catch (IndexOutOfBoundsException E) {
+                // do nothing
+            }
+        }        
+    }
+    
+    private int matrixToLinear(int x, int y){
+        return y+(x*3);
+    }
+    
+    private Tuple<Integer, Integer> linearToMatrix(int n){
+        Tuple<Integer, Integer> out = new Tuple<Integer,Integer>(n/3, n%3);
+        return out;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,211 @@
+import java.awt.*;
+import java.util.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main extends JFrame implements ActionListener {
+	private static final long serialVersionUID = 1;
+	JButton topRight, topMid, topLeft,
+            centerRight, center, centerLeft,
+            bottomRight, bottomMid, bottomLeft;
+    JButton undo, resetSeed, newSeed;
+	JPanel keypad, utilities, container, movecontainer;
+    JLabel movesLabel;
+    String moves = "Moves: ";
+    int movesNum = 0;
+
+    HivePuzzle puz = new HivePuzzle();
+
+    Icon barsIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\bars.png");
+    Icon AcrossIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\Across.png");
+    Icon radioIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\radio.png");
+    Icon bugIcon = new ImageIcon("C:\\Users\\ykozh\\Documents\\GitHub\\Hive-Puzzle-Sim\\bug.png");
+
+    private void updateBoard(HivePuzzle p){
+        topRight.setIcon(p.board[0].viewIcon());
+        topMid.setIcon(p.board[1].viewIcon());
+        topLeft.setIcon(p.board[2].viewIcon());
+        centerRight.setIcon(p.board[3].viewIcon());
+        center.setIcon(p.board[4].viewIcon());
+        centerLeft.setIcon(p.board[5].viewIcon());
+        bottomRight.setIcon(p.board[6].viewIcon());
+        bottomMid.setIcon(p.board[7].viewIcon());
+        bottomLeft.setIcon(p.board[8].viewIcon());
+        // topRight.setText(p.board[0].view());
+        // topMid.setText(p.board[1].view());
+        // topLeft.setText(p.board[2].view());
+        // centerRight.setText(p.board[3].view());
+        // center.setText(p.board[4].view());
+        // centerLeft.setText(p.board[5].view());
+        // bottomRight.setText(p.board[6].view());
+        // bottomMid.setText(p.board[7].view());
+        // bottomLeft.setText(p.board[8].view());
+    }
+
+	public Main() {
+		super("Hive Puzzle");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+		this.setLocation(dim.width / 2 - this.getSize().width / 2, dim.height / 2 - this.getSize().height / 2);
+        
+        container = new JPanel();
+        container.setLayout(new BoxLayout(container, BoxLayout.Y_AXIS));
+        
+        movesLabel = new JLabel(moves + movesNum, SwingConstants.LEFT);
+        movecontainer = new JPanel();
+        movecontainer.setLayout(new FlowLayout());
+        movecontainer.add(movesLabel);
+        
+		topRight = new JButton(barsIcon);
+        topRight.setPreferredSize(new Dimension(60,60));
+        topRight.setFont(new Font("Arial", Font.PLAIN, 20));
+        topRight.addActionListener(this);
+        
+        topMid = new JButton(barsIcon);
+        topMid.setPreferredSize(new Dimension(60,60));
+        topMid.setFont(new Font("Arial", Font.PLAIN, 20));
+        topMid.addActionListener(this);
+        
+        topLeft = new JButton(barsIcon);
+        topLeft.setPreferredSize(new Dimension(60,60));
+        topLeft.setFont(new Font("Arial", Font.PLAIN, 20));
+        topLeft.addActionListener(this);
+        
+        centerRight = new JButton(barsIcon);
+        centerRight.setPreferredSize(new Dimension(60,60));
+        centerRight.setFont(new Font("Arial", Font.PLAIN, 20));
+        centerRight.addActionListener(this);
+        
+        center = new JButton(barsIcon);
+        center.setPreferredSize(new Dimension(60,60));
+        center.setFont(new Font("Arial", Font.PLAIN, 20));
+        center.addActionListener(this);
+        
+        centerLeft = new JButton(barsIcon);
+        centerLeft.setPreferredSize(new Dimension(60,60));
+        centerLeft.setFont(new Font("Arial", Font.PLAIN, 20));
+        centerLeft.addActionListener(this);
+        
+        bottomRight = new JButton(barsIcon);
+        bottomRight.setPreferredSize(new Dimension(60,60));
+        bottomRight.setFont(new Font("Arial", Font.PLAIN, 20));
+        bottomRight.addActionListener(this);
+        
+        bottomMid = new JButton(barsIcon);
+        bottomMid.setPreferredSize(new Dimension(60,60));
+        bottomMid.setFont(new Font("Arial", Font.PLAIN, 20));
+        bottomMid.addActionListener(this);
+        
+        bottomLeft = new JButton(barsIcon);
+        bottomLeft.setPreferredSize(new Dimension(60,60));
+        bottomLeft.setFont(new Font("Arial", Font.PLAIN, 20));
+        bottomLeft.addActionListener(this);
+        
+        undo = new JButton("Undo");
+        undo.addActionListener(this);
+        resetSeed = new JButton("Reset");
+        resetSeed.addActionListener(this);
+        newSeed = new JButton("New");
+        newSeed.addActionListener(this);
+        
+        keypad = new JPanel();
+        keypad.setLayout(new GridLayout(3,3));
+        utilities = new JPanel();
+        
+        container.add(movecontainer);  
+        
+        keypad.add(topRight);
+        keypad.add(topMid);
+        keypad.add(topLeft);
+        keypad.add(centerRight);
+        keypad.add(center);
+        keypad.add(centerLeft);
+        keypad.add(bottomRight);
+        keypad.add(bottomMid);
+        keypad.add(bottomLeft);
+        
+        container.add(keypad);
+        
+        utilities.add(undo);
+        utilities.add(resetSeed);
+        utilities.add(newSeed);
+        
+        container.add(utilities);
+
+        add(container);
+        //setResizable(false);
+        //pack();
+        setSize(250, 300);
+		setVisible(true);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == undo) {
+            if (movesNum > 0){
+                movesNum--;
+                movesLabel.setText(moves + movesNum);   
+                puz.undo();
+                updateBoard(puz);
+            }
+		} else if (e.getSource() == topRight){ //TOP RIGHT
+            puz.makeMove(0,0);
+            movesNum++;            
+            movesLabel.setText(moves + movesNum);      
+            updateBoard(puz);
+        } else if (e.getSource() == topMid){ // TOP MID
+            puz.makeMove(0,1);            
+            movesNum++;
+            movesLabel.setText(moves + movesNum);   
+            updateBoard(puz);            
+        } else if (e.getSource() == topLeft){ // TOP LEFT
+            puz.makeMove(0,2);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == centerRight){ // CENTER RIGHT
+            puz.makeMove(1,0);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == center){ // CENTER
+            puz.makeMove(1,1);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == centerLeft){ // CENTER LEFT
+            puz.makeMove(1,2);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == bottomRight){ // BOTTOM RIGHT
+            puz.makeMove(2,0);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == bottomMid){ // BOTTOM MID
+            puz.makeMove(2,1);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == bottomLeft){ // BOTTOM LEFT
+            puz.makeMove(2,2);
+            movesNum++;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == resetSeed){ // RESET SEED
+            puz.resetSeed();
+            movesNum = 0;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        } else if (e.getSource() == newSeed){ // NEW SEED
+            puz.newSeed();
+            movesNum = 0;
+            movesLabel.setText(moves + movesNum);       
+            updateBoard(puz);
+        }
+	}
+
+	public static void main(String args[]) {
+		Main coo = new Main();
+	}
+}
